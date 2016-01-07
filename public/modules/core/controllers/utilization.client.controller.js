0b842668084fc7e1a2c3c21b1473923102f0ca17
Ashwin <narashwin@gmail.com>
2016-01-07 14:08:18 +0530

    Tables and Graph trial

--- public/modules/core/controllers/utilization.client.controller.js
+++ public/modules/core/controllers/utilization.client.controller.js
@@ -9,5 +9,66 @@ angular.module('core').controller('UtilizationController', ['$scope', 'Authentic
         // Some example string
         $scope.helloText = 'Utilization View';
         $scope.descriptionText = 'This shows the utilization of the resources';
-	}
+        
+        //Data For Table
+        $scope.ngData = [
+        {Name: "Moroni", Utilization: 50, Position: 'PR Menager', Status: 'active', Date: '12.12.2014'},
+        {Name: "Teancum", Utilization: 43, Position: 'CEO/CFO', Status: 'deactive', Date: '10.10.2014'},
+        {Name: "Jacob", Utilization: 27, Position: 'UI Designer', Status: 'active', Date: '09.11.2013'},
+        {Name: "Nephi", Utilization: 29, Position: 'Java programmer', Status: 'deactive', Date: '22.10.2014'},
+        {Name: "Joseph", Utilization: 22, Position: 'Marketing manager', Status: 'active', Date: '24.08.2013'},
+        {Name: "Monica", Utilization: 43, Position: 'President', Status: 'active', Date: '11.12.2014'},
+        {Name: "Arnold", Utilization: 12, Position: 'CEO', Status: 'active', Date: '07.10.2013'},
+        {Name: "Mark", Utilization: 54, Position: 'Analyst', Status: 'deactive', Date: '03.03.2014'},
+        {Name: "Amelia", Utilization: 33, Position: 'Sales manager', Status: 'deactive', Date: '26.09.2013'},
+        {Name: "Jesica", Utilization: 41, Position: 'Ruby programmer', Status: 'active', Date: '22.12.2013'},
+        {Name: "John", Utilization: 48, Position: 'Marketing manager', Status: 'deactive', Date: '09.10.2014'},
+        {Name: "Berg", Utilization: 19, Position: 'UI/UX Designer', Status: 'active', Date: '12.11.2013'}
+    ];
+
+    $scope.utilTable = { data: 'ngData' };
+        
+        //Data for Line Chart
+    $scope.lineData = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "Example dataset",
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "Example dataset",
+                fillColor: "rgba(26,179,148,0.5)",
+                strokeColor: "rgba(26,179,148,0.7)",
+                pointColor: "rgba(26,179,148,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(26,179,148,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+        //Line Options for line chart
+    $scope.lineOptions = {
+        scaleShowGridLines : true,
+        scaleGridLineColor : "rgba(0,0,0,.05)",
+        scaleGridLineWidth : 1,
+        bezierCurve : true,
+        bezierCurveTension : 0.4,
+        pointDot : true,
+        pointDotRadius : 4,
+        pointDotStrokeWidth : 1,
+        pointHitDetectionRadius : 20,
+        datasetStroke : true,
+        datasetStrokeWidth : 2,
+        datasetFill : true
+    };
+        
+   	}
 ]);
