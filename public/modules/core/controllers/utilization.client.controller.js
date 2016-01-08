fb7efc6729e64dbcb8bfcc1b3992630824a8263c
Ashwin <narashwin@gmail.com>
2016-01-08 14:17:22 +0530

    View Updations

--- public/modules/core/controllers/utilization.client.controller.js
+++ public/modules/core/controllers/utilization.client.controller.js
@@ -67,6 +67,45 @@ angular.module('core').controller('UtilizationController', ['$scope', 'Authentic
         datasetStrokeWidth : 2,
         datasetFill : true
     };
+        this.polarData = [
+        {
+            value: 300,
+            color:"#a3e1d4",
+            highlight: "#1ab394",
+            label: "App"
+        },
+        {
+            value: 140,
+            color: "#dedede",
+            highlight: "#1ab394",
+            label: "Software"
+        },
+        {
+            value: 200,
+            color: "#A4CEE8",
+            highlight: "#1ab394",
+            label: "Laptop"
+        }
+    ];
+
+    /**
+     * Options for Polar chart
+     */
+    this.polarOptions = {
+        scaleShowLabelBackdrop : true,
+        scaleBackdropColor : "rgba(255,255,255,0.75)",
+        scaleBeginAtZero : true,
+        scaleBackdropPaddingY : 1,
+        scaleBackdropPaddingX : 1,
+        scaleShowLine : true,
+        segmentShowStroke : true,
+        segmentStrokeColor : "#fff",
+        segmentStrokeWidth : 2,
+        animationSteps : 100,
+        animationEasing : "easeOutBounce",
+        animateRotate : true,
+        animateScale : false
+    };
       
    	}
 ]);
