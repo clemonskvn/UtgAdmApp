daf0233b975e02ad43334e1d728bbda018f2890f
Ashwin <narashwin@gmail.com>
2016-01-08 17:04:51 +0530

    View Updations

--- public/modules/core/controllers/projectDashboard.client.controller.js
+++ public/modules/core/controllers/projectDashboard.client.controller.js
@@ -9,5 +9,16 @@ angular.module('core').controller('ProjectController', ['$scope', 'Authenticatio
         // Some example string
         $scope.helloText = 'Project Dashboard';
         $scope.descriptionText = 'Project Dashboards';
+        
+        $('#datetimepicker6').datetimepicker();
+        $('#datetimepicker7').datetimepicker({
+            useCurrent: false //Important! See issue #1075
+        });
+        $("#datetimepicker6").on("dp.change", function (e) {
+            $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+        });
+        $("#datetimepicker7").on("dp.change", function (e) {
+            $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+        });
 	}
 ]);
