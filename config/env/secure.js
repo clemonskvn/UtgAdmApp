f2f59b8950e42017599a8bc00a41b9331a7843e5
Ashwin <narashwin@gmail.com>
2016-01-11 20:36:29 +0530

    Bower updates

--- config/env/secure.js
+++ config/env/secure.js
@@ -15,7 +15,8 @@ module.exports = {
 				'public/lib/angular-animate/angular-animate.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+                'public/lib/Chart.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
