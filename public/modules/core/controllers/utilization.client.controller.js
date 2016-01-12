f532cf38d70044205c7cd897562b8a883348e9a4
Ashwin <narashwin@gmail.com>
2016-01-12 17:40:28 +0530

    charts

--- public/modules/core/controllers/utilization.client.controller.js
+++ public/modules/core/controllers/utilization.client.controller.js
@@ -27,7 +27,7 @@ angular.module('core').controller('UtilizationController', ['$scope', 'Authentic
     $scope.utilTable = { data: 'ngData' };
         
         //Data for Line Chart
-        var data = {
+    var data = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
         {
@@ -66,57 +66,85 @@ angular.module('core').controller('UtilizationController', ['$scope', 'Authentic
         datasetStrokeWidth : 2,
         datasetFill : true
     };        
+        
     var ctx = document.getElementById("myChart").getContext("2d");
     var myLineChart = new Chart(ctx).Line(data, options);
         
-   /**
-     * Data for Line chart
-     */
-    this.lineData = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "Example dataset",
-                fillColor: "rgba(220,220,220,0.5)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: "Example dataset",
-                fillColor: "rgba(26,179,148,0.5)",
-                strokeColor: "rgba(26,179,148,0.7)",
-                pointColor: "rgba(26,179,148,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(26,179,148,1)",
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-        ]
-    };
+   // Data and options for Polar Chart
+    var polarOptions={
+    //Boolean - Show a backdrop to the scale label
+    scaleShowLabelBackdrop : true,
 
-    /**
-     * Options for Line chart
-     */
-    this.lineOptions = {
-        scaleShowGridLines : true,
-        scaleGridLineColor : "rgba(0,0,0,.05)",
-        scaleGridLineWidth : 1,
-        bezierCurve : true,
-        bezierCurveTension : 0.4,
-        pointDot : true,
-        pointDotRadius : 4,
-        pointDotStrokeWidth : 1,
-        pointHitDetectionRadius : 20,
-        datasetStroke : true,
-        datasetStrokeWidth : 2,
-        datasetFill : true
-    };
-   /* var ctx = document.getElementById("lineChart").getContext('2d');
-    var myLineChart= new Chart(ctx).Line(data,option);*/
+    //String - The colour of the label backdrop
+    scaleBackdropColor : "rgba(255,255,255,0.75)",
+
+    // Boolean - Whether the scale should begin at zero
+    scaleBeginAtZero : true,
+
+    //Number - The backdrop padding above & below the label in pixels
+    scaleBackdropPaddingY : 2,
+
+    //Number - The backdrop padding to the side of the label in pixels
+    scaleBackdropPaddingX : 2,
+
+    //Boolean - Show line for each value in the scale
+    scaleShowLine : true,
+
+    //Boolean - Stroke a line around each segment in the chart
+    segmentShowStroke : true,
+
+    //String - The colour of the stroke on each segement.
+    segmentStrokeColor : "#fff",
+
+    //Number - The width of the stroke value in pixels
+    segmentStrokeWidth : 2,
+
+    //Number - Amount of animation steps
+    animationSteps : 100,
+
+    //String - Animation easing effect.
+    animationEasing : "easeOutBounce",
+
+    //Boolean - Whether to animate the rotation of the chart
+    animateRotate : true,
+
+    //Boolean - Whether to animate scaling the chart from the centre
+    animateScale : false,
+
+    };    
+    var polarData = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    },
+    {
+        value: 40,
+        color: "#949FB1",
+        highlight: "#A8B3C5",
+        label: "Grey"
+    },
+    {
+        value: 120,
+        color: "#4D5360",
+        highlight: "#616774",
+        label: "Dark Grey"
+    }
+
+];
        
         this.polarData = [
         {
@@ -138,7 +166,8 @@ angular.module('core').controller('UtilizationController', ['$scope', 'Authentic
             label: "Laptop"
         }
     ];
-
+    var ctx2 = document.getElementById("myPolarChart").getContext("2d");    
+    var myPolarChart = new Chart(ctx2).PolarArea(polarData, options);    
     /**
      * Options for Polar chart
      */
