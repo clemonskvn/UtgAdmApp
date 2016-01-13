e719f73d9d8058a2916e97c94d4aa175ee664475
Ashwin <narashwin@gmail.com>
2016-01-13 13:56:05 +0530

    Flot Charts

--- public/modules/core/controllers/projectDashboard.client.controller.js
+++ public/modules/core/controllers/projectDashboard.client.controller.js
@@ -97,6 +97,44 @@ angular.module('core').controller('ProjectController', ['$scope', 'Authenticatio
             show: false
         }
     };
+    
+    $scope. floatLineOptions = {
+        series: {
+            lines: {
+                show: true,
+                lineWidth: 2,
+                fill: true,
+                fillColor: {
+                    colors: [
+                        {
+                            opacity: 0.0
+                        },
+                        {
+                            opacity: 0.0
+                        }
+                    ]
+                }
+            }
+        },
+        xaxis: {
+            tickDecimals: 0
+        },
+        colors: ["#1ab394"],
+        grid: {
+            color: "#999999",
+            hoverable: true,
+            clickable: true,
+            tickColor: "#D4D4D4",
+            borderWidth: 0
+        },
+        legend: {
+            show: false
+        },
+        tooltip: true,
+        tooltipOpts: {
+            content: "x: %x, y: %y"
+        }
+    };
         
 	}
 ]);
